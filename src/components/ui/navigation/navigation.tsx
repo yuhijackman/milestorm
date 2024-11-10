c8b22d83cb38a822695e58f9fdbaa73a3dab521a
yuhijackman <yuuhiandlisa@gmail.com>
2024-11-10 20:39:34 +0900

    Create Component: Navigation

---
+++ src/components/ui/navigation/navigation.tsx
@@ -0,0 +1,16 @@
+import Link from 'next/link';
+import { MaxWidthWrapper } from '../max-width-wrapper';
+
+export const Navigation = () => {
+  return (
+    <nav className="sticky z-[100] h-14 inset-x-0 top-0 w-full border-b border-gray-200 bg-white/80 transition-all">
+      <MaxWidthWrapper>
+        <div className="flex h-14 items-center justify-between">
+          <Link href="/" className="flex z-40 text-xl font-bold text-primary">
+            Milestorm
+          </Link>
+        </div>
+      </MaxWidthWrapper>
+    </nav>
+  );
+};
