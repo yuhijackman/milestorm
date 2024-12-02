f979a93819bd5fa8faa5ffcf753ae7903fa4dc0c
yuhijackman <yuuhiandlisa@gmail.com>
2024-12-02 20:53:32 +0900

    ボタンコンポーネントの調整

--- src/components/ui/button/button.tsx
+++ src/components/ui/button/button.tsx
@@ -1,7 +1,7 @@
 import { Slot } from '@radix-ui/react-slot';
 import * as React from 'react';
 import { cva, type VariantProps } from 'class-variance-authority';
-import { cn } from '@/utils/cn';
+import { cn } from '@/lib/utils';
 
 const buttonVariants = cva(
   'inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50',
@@ -31,6 +31,7 @@ const buttonVariants = cva(
     },
   },
 );
+
 export interface ButtonProps
   extends React.ButtonHTMLAttributes<HTMLButtonElement>,
     VariantProps<typeof buttonVariants> {
